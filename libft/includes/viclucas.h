50c13108954cefc9d05de70f231a01668cf9556a
Jossy RASOLOHARIJAONA <jrasoloh@e1r8p14.42.fr>
2018-08-16 06:19:40 +0200

    Fix norm in header

--- libft/includes/viclucas.h
+++ libft/includes/viclucas.h
@@ -12,8 +12,9 @@
 
 #ifndef VICLUCAS_H
 # define VICLUCAS_H
-#include <sys/stat.h>
-#include <stdlib.h>
+# include <sys/stat.h>
+# include <stdlib.h>
+
 typedef struct		s_tabv
 {
 	int		flag1;
@@ -24,12 +25,11 @@ typedef struct		s_tabv
 	char	**board;
 }					t_tabv;
 
-
-char		**ft_free_db_tab(char **av);
-char		*ft_souder(char *s1, char *s2, char *s3);
-int			ft_error(char *s1, char *name);
-void	ft_quotes_check(t_tabv *x, char c);
-char	*ft_acces(char *s);
-void	print_db(char **board);
-void	ft_free_thoses(char **s1, char **s2, char **s3, char **s4);
+char				**ft_free_db_tab(char **av);
+char				*ft_souder(char *s1, char *s2, char *s3);
+int					ft_error(char *s1, char *name);
+void				ft_quotes_check(t_tabv *x, char c);
+char				*ft_acces(char *s);
+void				print_db(char **board);
+void				ft_free_thoses(char **s1, char **s2, char **s3, char **s4);
 #endif
